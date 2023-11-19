2b328508b1512f5807670449affe4a5cfa806227
vamel <79980481+vamel@users.noreply.github.com>
2023-11-19 23:02:15 +0100

    added config file for firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDj27VHnrRqnuy81dVkvuzeNWf-jjWBwOg",
+  authDomain: "travel-app-bd756.firebaseapp.com",
+  projectId: "travel-app-bd756",
+  storageBucket: "travel-app-bd756.appspot.com",
+  messagingSenderId: "640595299801",
+  appId: "1:640595299801:web:64f54a94404dd37372ddc8",
+  measurementId: "G-9SBY60B2BJ"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
