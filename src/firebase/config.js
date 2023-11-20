b779b368c015e8068bf9e3387d48d6c1e0384c0f
vamel <79980481+vamel@users.noreply.github.com>
2023-11-20 18:32:55 +0100

    Added firebase storage as dependency

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,13 +2,8 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-// import { getAnalytics } from "firebase/analytics";
+import { getStorage } from "firebase/storage";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDj27VHnrRqnuy81dVkvuzeNWf-jjWBwOg",
   authDomain: "travel-app-bd756.firebaseapp.com",
@@ -21,8 +16,8 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
-// const analytics = getAnalytics(app);
 
-export default {app, auth, db};
+// Export components
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
