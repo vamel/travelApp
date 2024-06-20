49ab3cf3c36b22393a16de43850c981fd5346e2c
vamel <79980481+vamel@users.noreply.github.com>
2024-06-20 16:38:11 +0200

    Added navigation

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { useFonts } from "expo-font";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 import AttractionList from './src/components/main/AttractionList';
 import AttractionPage from "./src/components/attraction/AttractionPage";
 import LoginPage from "./src/components/main/LoginPage";
@@ -9,17 +10,29 @@ import WelcomePage from "./src/components/main/WelcomePage";
 
 export default function App() {
 
+  const Drawer = createDrawerNavigator();
   const Stack = createNativeStackNavigator();
   let [fontsLoaded] = useFonts({
     'Roboto': require('./src/assets/fonts/Roboto/Roboto-Regular.ttf')
   });
+
+  const Root = () => {
+      // @ts-ignore
+      return (
+        <Drawer.Navigator>
+            <Drawer.Screen name={"WelcomePage"} component={WelcomePage} options={{title: "Home"}}/>
+            <Drawer.Screen name={"AttractionList"} component={AttractionList} options={{title: "Attraction List"}}/>
+        </Drawer.Navigator>
+      );
+  }
 
   // @ts-ignore
   return (
       <>
       <StatusBar style={"dark"}/>
       <NavigationContainer>
-              <Stack.Navigator initialRouteName={"LoginPage"}>
+              <Stack.Navigator>
+                  <Stack.Screen name={"Root"} component={Root} options={{headerShown: false}}/>
                   <Stack.Screen name={"LoginPage"} component={LoginPage} options={{title: "Login Page"}}/>
                   <Stack.Screen name={"WelcomePage"} component={WelcomePage} options={{title: "Home"}}/>
                   <Stack.Screen name={"AttractionList"} component={AttractionList} options={{title: "Attraction List"}}/>
