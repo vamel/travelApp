18816cf7b0265d22c6646f0d32bbf243d620b137
vamel <79980481+vamel@users.noreply.github.com>
2024-08-25 03:05:15 +0200

    Removed unused import and moved function to dateUtils

--- src/components/event/EventCard.tsx
+++ src/components/event/EventCard.tsx
@@ -1,24 +1,31 @@
-import {View, Text} from "react-native";
+import {View, Text, Pressable} from "react-native";
 import EventDateCard from "./EventDateCard";
 import {eventCardStyles} from "../../styles/components/EventCardStyles";
+import {IEvent} from "../../models/interfaces/IEvent";
 
 interface IEventCardProps {
-    date: string,
-    location: string,
-    name: string
+    eventData: IEvent,
+    onPress: (event: any) => void
 }
 
 const EventCard = (props: IEventCardProps) => {
-
     return(
-        <View style={eventCardStyles.eventCard}>
-            <View>
-                <EventDateCard date={props.date} />
-            </View>
-            <View style={eventCardStyles.textContainer}>
-                <Text style={eventCardStyles.titleText}>{props.name}</Text>
-                <Text style={eventCardStyles.locationText}>at POLIN museum</Text>
-            </View>
+        <View style={eventCardStyles.container}>
+            <Pressable
+                onPress={props.onPress.bind(this, props.eventData)}
+                android_ripple={eventCardStyles.rippleAndroid}
+                style={({pressed}) => pressed ? eventCardStyles.buttonPressed : null}
+            >
+                <View style={eventCardStyles.eventCard}>
+                    <View>
+                        <EventDateCard date={props.eventData.date} />
+                    </View>
+                    <View style={eventCardStyles.textContainer}>
+                        <Text style={eventCardStyles.titleText}>{props.eventData.name}</Text>
+                        <Text style={eventCardStyles.locationText}>at POLIN museum</Text>
+                    </View>
+                </View>
+            </Pressable>
         </View>
     );
 }
--- src/components/event/EventDateCard.tsx
+++ src/components/event/EventDateCard.tsx
@@ -1,28 +1,13 @@
 import {View, Text} from "react-native";
 import {eventDateCardStyles} from "../../styles/components/EventDateCardStyles";
+import {getDateElements, getDayOrdinalSuffix} from "../utils/dateUtils";
 
 const EventDateCard = (props) => {
-    const displayDate = props.date.toDate();
-    const day = displayDate.getDay();
-    const month = displayDate.toLocaleDateString("en-GB", {month: "short"});
-    const year = displayDate.getFullYear();
-
-    const getDayOrdinalSuffix = () => {
-        switch (day) {
-            case [1, 21, 31].includes(day):
-                return 'st'
-            case [2, 22].includes(day):
-                return 'nd'
-            case [3, 23].includes(day):
-                return 'rd'
-            default:
-                return 'th'
-        }
-    }
+    const [day, month, year] = getDateElements(props.date);
 
     return(
         <View style={eventDateCardStyles.container}>
-            <Text style={eventDateCardStyles.text}>{`${month} ${day}${getDayOrdinalSuffix()}`}</Text>
+            <Text style={eventDateCardStyles.text}>{`${month} ${getDayOrdinalSuffix(day)}`}</Text>
             <Text style={eventDateCardStyles.text}>{year}</Text>
         </View>
     );
