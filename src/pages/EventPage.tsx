d27dfb16b804b56b0ac95d50c7105a72cfb2a40c
vamel <79980481+vamel@users.noreply.github.com>
2024-08-25 03:02:01 +0200

    Changed EventPage.tsx

--- src/pages/EventPage.tsx
+++ src/pages/EventPage.tsx
@@ -1,10 +1,63 @@
-import {View, Text} from "react-native";
+import {View, Text, SafeAreaView, ScrollView} from "react-native";
+import {useEffect, useState} from "react";
+import {attractionPageStyles} from "../styles/pages/AttractionPageStyles";
+import {dateToString, getDateElements} from "../components/utils/dateUtils";
 
-const EventPage = () => {
+const EventPage = ({route}) => {
+    const [eventData, setEventData] = useState(null);
+    const [dateString, setDateString] = useState("");
+
+    const receivedData = {
+        name: route.params.name,
+        description: route.params.description,
+        date: route.params.date
+    };
+
+    useEffect(() => {
+        const [day, month, year] = getDateElements(route.params.date);
+        const dateString = dateToString(day, month, year);
+        setDateString(dateString);
+        //@ts-ignore
+        setEventData(receivedData);
+        console.log(eventData);
+    }, [receivedData.name]);
+
+    if (!eventData) {
+        return(
+            <View>
+                <Text>
+                    Loading...
+                </Text>
+            </View>
+        );
+    }
+
     return(
-        <View>
-            <Text>Event page</Text>
-        </View>
+        <SafeAreaView style={attractionPageStyles.attractionPage}>
+            <ScrollView
+                showsVerticalScrollIndicator={false}
+            >
+                <View>
+                    <Text style={attractionPageStyles.title}>
+                        {eventData.name}
+                    </Text>
+                </View>
+                <View style={attractionPageStyles.detailsContainer}>
+                    <Text style={attractionPageStyles.detailsTitle}>Location:</Text>
+                    <Text style={attractionPageStyles.detailsText}>
+                        LOCATION PLACEHOLDER
+                    </Text>
+                    <Text style={attractionPageStyles.detailsTitle}>Date:</Text>
+                    <Text style={attractionPageStyles.detailsText}>
+                        {dateString}
+                    </Text>
+                    <Text style={attractionPageStyles.detailsTitle}>Description:</Text>
+                    <Text style={attractionPageStyles.detailsText}>
+                        {eventData.description}
+                    </Text>
+                </View>
+            </ScrollView>
+        </SafeAreaView>
     );
 }
 
